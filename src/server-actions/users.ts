876515aa3dc800e4dbaee291e1f2ae28e25c7f3e
e.karimi <abnoseek@gmail.com>
2025-04-14 16:21:22 +0330

    feat: add user model

---
+++ src/server-actions/users.ts
@@ -0,0 +1,29 @@
+import { connectDB } from "@/config/db";
+import UserModel from "@/models/user-model";
+import { currentUser, EmailAddress } from "@clerk/nextjs/server";
+export const getCurrentUserFromMongoDB = async () => {
+  try {
+    const clerkUser = await currentUser();
+
+    const mongoUser = await UserModel.findOne({ clerkUserId: clerkUser?.id });
+
+    if (mongoUser) {
+      return JSON.parse(JSON.stringify(mongoUser));
+    }
+
+    const newUserPayload = {
+      clerkUserId: clerkUser?.id,
+      name: clerkUser?.firstName + " " + clerkUser?.lastName,
+      userName: clerkUser?.username,
+      email: clerkUser?.emailAddresses[0].emailAddress || "",
+      profilePicture: clerkUser?.imageUrl || "",
+    };
+
+    const newUser = await UserModel.create(newUserPayload);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error: any) {
+    return {
+      error: error.message,
+    };
+  }
+};
