2e97de08157ae10e9d6123cdb3feb5d74a417fb2
e.karimi <abnoseek@gmail.com>
2025-04-19 12:21:45 +0330

    feat: add interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,3 +8,29 @@ export interface IUserType {
   updatedAt: string;
   userName: string;
 }
+
+export interface IChatType {
+  _id: string;
+  users: IUserType[];
+  createdBy: IUserType;
+  lastMessage: IMessageType;
+  isGroupChat: boolean;
+  groupName: string;
+  groupProfilePicture: string;
+  groupBio: string;
+  groupAdmins: IUserType[];
+  unreadCounts: Record<string, number>;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IMessageType {
+  _id: string;
+  chat: IChatType;
+  sender: IUserType;
+  text: string;
+  image: string;
+  readBy: IUserType[];
+  createdAt: string;
+  updatedAt: string;
+}
